1903518b9ce3f5d6719921922c87c480f0769da0
RMEC11 <abhishek.designmate@gmail.com>
2025-08-04 13:06:58 +0530

    Initial commit

---
+++ Test/test_Login.py
@@ -0,0 +1,22 @@
+import pytest
+
+from PageObject.signUp_signIn import signIn_up
+from Utilities.utility import utility
+
+
+class Test_Embibe_Login(utility):
+
+
+    @pytest.mark.usefixtures("setup", "log_on_failure")
+    def test_sign_in_password(self):
+        login = signIn_up(self.driver)
+        login.test_sign_in_password()
+        log = self.getLogger()
+        log.info("Testcase: Sign in with Password")
+
+    @pytest.mark.usefixtures("setup", "log_on_failure")
+    def test_signIn_mobile(self):
+        log = self.getLogger()
+        login = signIn_up(self.driver)
+        login.sign_in_with_mobile()
+        log.info("Testcase: Sign in with Mobile number")
