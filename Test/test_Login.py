ac00dc21492afc720256cfa8e4952e9c7600caa0
RMEC11 <abhishek.designmate@gmail.com>
2025-08-04 13:12:39 +0530

    2nd Initial commit

--- Test/test_Login.py
+++ Test/test_Login.py
@@ -6,6 +6,7 @@ from Utilities.utility import utility
 
 class Test_Embibe_Login(utility):
 
+#Git Upload/Push the code
 
     @pytest.mark.usefixtures("setup", "log_on_failure")
     def test_sign_in_password(self):
